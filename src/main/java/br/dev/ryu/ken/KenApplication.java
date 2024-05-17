bfe90c89de8dd24fdbf68cac14c08fd792510fa3
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-05-17 12:42:36 -0300

    first commit

---
+++ src/main/java/br/dev/ryu/ken/KenApplication.java
@@ -0,0 +1,13 @@
+package br.dev.ryu.ken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/ryu/ken/KenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ryu.ken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
