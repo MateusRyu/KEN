788a92679bdc831fcd89b009749c4a06cbcfd771
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-05-31 02:36:10 -0300

    feat: create book Format Class

---
+++ src/main/java/br/dev/ryu/ken/model/Format.java
@@ -0,0 +1,15 @@
+package br.dev.ryu.ken.model;
+
+import java.util.Map;
+
+public class Format {
+    private Map<String, String> formatMap;
+
+    public Map<String, String> getFormatMap() {
+        return formatMap;
+    }
+
+    public void setFormatMap(Map<String, String> formatMap) {
+        this.formatMap = formatMap;
+    }
+}
