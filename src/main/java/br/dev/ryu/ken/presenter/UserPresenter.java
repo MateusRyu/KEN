2d34a1d6c4ac8601d34a84cb9e0ac69e73f594ed
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-06-02 02:20:47 -0300

    feat: Implement register book (by id and by title)

--- src/main/java/br/dev/ryu/ken/presenter/UserPresenter.java
+++ src/main/java/br/dev/ryu/ken/presenter/UserPresenter.java
@@ -22,7 +22,7 @@ public class UserPresenter {
         return VIEW.displayStartMenu();
     }
 
-    public void getBookByTitle() {
+    public void getAllBookByTitle() {
         System.out.println("What's the book's title?");
         String title = SCANNER.nextLine();
         System.out.println("Searching..");
@@ -34,5 +34,33 @@ public class UserPresenter {
             List<Book> books = API.getBooksFromListOfBooks(listOfBooks);
             books.forEach(System.out::println);
         }
+    }
+
+    private void registerBook(boolean confirm, Book book) {
+        if (confirm) {
+            // TODO implement to save book
+            System.out.println("Registering " + book + "...");
+        }
+    }
+
+    public void registerBookByTitle() {
+        System.out.println("What's the book's title?");
+        String title = SCANNER.nextLine();
+        System.out.println("Searching..");
+        ListOfBooks listOfBooks = API.getBooksByTitle(title);
+        System.out.println("Books found: " + listOfBooks.count());
+        Book book = new Book(listOfBooks.books().getFirst());
+        System.out.println("Most popular book with this title: " + book);
+        boolean confirm = VIEW.confirm("Want to register " + book + "?");
+        registerBook(confirm, book);
+    }
+
+    public void registerBookById() {
+        System.out.println("What's the book's ID?");
+        String title = SCANNER.nextLine();
+        System.out.println("Searching..");
+        Book book = new Book(API.getBooksById(title));
+        boolean confirm = VIEW.confirm("Want to register " + book + "?");
+        registerBook(confirm, book);
     }
 }
